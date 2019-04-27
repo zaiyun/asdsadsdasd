af41d0fa0c91a98a0de414b8b3a3739b0192def0
lin <zl1836@nyu.edu>
2019-04-26 21:51:33 -0400

    Crouch, pop out trigger

---
+++ midterm_fps/Assets/Script/Crouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    CharacterController characterCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterCollider = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            characterCollider.height = 1;
+        }
+        else
+        {
+            if (characterCollider.height <= 2) {
+                characterCollider.height += 0.25f;
+            }
+
+        }
+    }
+}
---
+++ midterm_fps/Assets/Script/PopOutScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopOutScript : MonoBehaviour
+{
+    private Transform popoutObj;
+    BoxCollider coll;
+    float length;
+    Vector3 position;
+    public float popOutDistance;
+    bool pop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        popoutObj = this.transform.Find("PopOut");
+        //coll = popout.GetComponent<BoxCollider>();
+        //length = coll.bounds.size.y;
+        position = popoutObj.localPosition;
+        //print(length);
+        //position.y = 0 - (length / 2.0f);
+        //popout.localPosition = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pop)
+        {
+        
+            position = popoutObj.localPosition;
+            position.y = popOutDistance;
+            popoutObj.localPosition = position;
+
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "projectile")
+        {
+            pop = !pop;
+        }
+    }
+}
