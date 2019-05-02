794a1a75dd82eb9163b7b1df4a0e5b6d6f14be1e
JunLin Li <jl8278@nyu.edu>
2019-05-02 16:38:39 -0400

    added Lazer, table rework

---
+++ midterm_fps/Assets/JayStuff/JayScript/LazerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LazerScript : MonoBehaviour
+{
+    
+    private LineRenderer Lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+
+            if (hit.collider)
+            {
+
+                Lr.SetPosition(1, hit.point);
+
+            }
+
+            if (hit.collider.tag == "Player") {
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            }
+
+        }
+        else Lr.SetPosition(1, transform.forward * 5000);
+    }
+}
