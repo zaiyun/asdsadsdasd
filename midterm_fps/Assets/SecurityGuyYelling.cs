aa07ef80d6ecb849db41140ae2c55fc0089e81db
lin <zl1836@nyu.edu>
2019-05-05 15:42:26 -0400

    no message

---
+++ midterm_fps/Assets/SecurityGuyYelling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityGuyYelling : MonoBehaviour
+{
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if (this.GetComponentInParent<collsionTrigger>().hit == false)
+        {
+            audio.PlayOneShot(audio.clip);
+        }
+    }
+}
